cbff67842fefc8cadf1c6e701b03f404686dcd21
Hyojin Kim <hyojins.kim@samsung.com>
2020-03-11 15:06:51 +0900

    Create empty file function

--- command/init.js
+++ command/init.js
@@ -1,5 +1,15 @@
+const util = require("../lib/util.js");
+
+const WITS_CONFIG_FILE_NAME = ".witsconfig.json";
+const WITS_IGNORE_FILE_NAME = ".witsignore";
+
 module.exports = {
     run: () => {
-        console.log("init command run!");
+        createWitsconfigFile();
     }
 };
+
+function createWitsconfigFile() {
+    util.createEmptyFile(util.CURRENT_PROJECT_PATH, WITS_CONFIG_FILE_NAME);
+    util.createEmptyFile(util.CURRENT_PROJECT_PATH, WITS_IGNORE_FILE_NAME);
+}
