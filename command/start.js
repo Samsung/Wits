4b0084c4a7a5a5e61d84abb30dba9796d7ec85e2
woosik.park <woosik.park@samsung.com>
2022-10-28 10:06:53 +0900

    Add deviceConnectPort option

--- command/start.js
+++ command/start.js
@@ -7,80 +7,83 @@ const watchHelper = require('../lib/watchHelper.js');
 const { logger } = require('../lib/logger');
 
 module.exports = {
-    run: async option => {
-        logger.log(chalk.cyanBright(`Start running Wits............\n`));
+  run: async option => {
+    logger.log(chalk.cyanBright(`Start running Wits............\n`));
 
-        await util.initTools();
+    await util.initTools();
 
-        const data = userInfoHelper.getRefinedData();
+    const data = userInfoHelper.getRefinedData();
 
-        if (option !== undefined) {
-            await supportDeviceIpOption(data, option);
-        }
-
-        let deviceInfo = '';
-
-        try {
-            deviceInfo = await userInfoHelper.getDeviceInfo(data.deviceIp);
-        } catch (error) {
-            logger.log(`Failed to getDeviceInfo: ${error}`);
-        }
-
-        await hostAppHelper.setHostAppEnv(data, deviceInfo);
-        hostAppHelper
-            .buildPackage()
-            .then(() => {
-                logger.log(
-                    chalk.cyanBright(
-                        '============================== Start to install the package'
-                    )
-                );
-
-                const hostAppId = hostAppHelper.getHostAppId(data.baseAppPath);
-                const hostAppName = hostAppId.split('.')[1];
-                const deviceName = deviceInfo.deviceName;
-
-                appLaunchHelper.unInstallPackage(deviceName, hostAppName);
-                appLaunchHelper.installPackage(deviceInfo, hostAppName);
-                watchHelper.openSocketServer(data, deviceInfo);
-                try {
-                    data.isDebugMode
-                        ? appLaunchHelper.launchDebugMode(
-                              deviceName,
-                              hostAppId,
-                              data.deviceIp
-                          )
-                        : appLaunchHelper.launchApp(deviceName, hostAppId);
-                } catch (e) {
-                    logger.log(e);
-                    util.exit();
-                }
-            })
-            .catch(e => {
-                logger.error(chalk.red(`Failed to buildPackage: ${e}`));
-                util.exit();
-            });
+    if (option !== undefined) {
+      await supportDeviceIpOption(data, option);
     }
+
+    let deviceInfo = '';
+
+    try {
+      deviceInfo = await userInfoHelper.getDeviceInfo(
+        data.deviceIp,
+        data.deviceConnectPort
+      );
+    } catch (error) {
+      logger.log(`Failed to getDeviceInfo: ${error}`);
+    }
+
+    await hostAppHelper.setHostAppEnv(data, deviceInfo);
+    hostAppHelper
+      .buildPackage()
+      .then(() => {
+        logger.log(
+          chalk.cyanBright(
+            '============================== Start to install the package'
+          )
+        );
+
+        const hostAppId = hostAppHelper.getHostAppId(data.baseAppPath);
+        const hostAppName = hostAppId.split('.')[1];
+        const deviceName = deviceInfo.deviceName;
+
+        appLaunchHelper.unInstallPackage(deviceName, hostAppName);
+        appLaunchHelper.installPackage(deviceInfo, hostAppName);
+        watchHelper.openSocketServer(data, deviceInfo);
+        try {
+          data.isDebugMode
+            ? appLaunchHelper.launchDebugMode(
+                deviceName,
+                hostAppId,
+                data.deviceIp
+              )
+            : appLaunchHelper.launchApp(deviceName, hostAppId);
+        } catch (e) {
+          logger.log(e);
+          util.exit();
+        }
+      })
+      .catch(e => {
+        logger.error(chalk.red(`Failed to buildPackage: ${e}`));
+        util.exit();
+      });
+  }
 };
 
 async function supportDeviceIpOption(data, option) {
-    const optionDeviceIp = util.parseDeviceIp(option);
-    if (optionDeviceIp === null && typeof option !== 'boolean') {
-        logger.error(
-            chalk.red(
-                `Invalid Type of cli option. Please retry with correct type. ex) deviceIp=0.0.0.0`
-            )
-        );
-        util.exit();
-    }
+  const optionDeviceIp = util.parseDeviceIp(option);
+  if (optionDeviceIp === null && typeof option !== 'boolean') {
+    logger.error(
+      chalk.red(
+        `Invalid Type of cli option. Please retry with correct type. ex) deviceIp=0.0.0.0`
+      )
+    );
+    util.exit();
+  }
 
-    if (optionDeviceIp) {
-        data.deviceIp = optionDeviceIp;
-        logger.log(
-            chalk.cyanBright(`WITs tries connecting with ${optionDeviceIp}\n`)
-        );
-        await userInfoHelper.updateLatestUserAnswer({
-            deviceIp: optionDeviceIp
-        });
-    }
+  if (optionDeviceIp) {
+    data.deviceIp = optionDeviceIp;
+    logger.log(
+      chalk.cyanBright(`WITs tries connecting with ${optionDeviceIp}\n`)
+    );
+    await userInfoHelper.updateLatestUserAnswer({
+      deviceIp: optionDeviceIp
+    });
+  }
 }
--- command/watch.js
+++ command/watch.js
@@ -7,63 +7,59 @@ const chalk = require('chalk');
 const { logger } = require('../lib/logger');
 
 module.exports = {
-    run: async option => {
-        logger.log(
-            chalk.cyanBright(`Start running Wits watch mode............\n`)
-        );
+  run: async option => {
+    logger.log(chalk.cyanBright(`Start running Wits watch mode............\n`));
 
-        await util.initTools();
+    await util.initTools();
 
-        const data = await userInfoHelper.getLatestWitsconfigInfo()
-            .connectionInfo;
+    const data = await userInfoHelper.getLatestWitsconfigInfo().connectionInfo;
 
-        if (option !== undefined) {
-            await supportDeviceIpOption(data, option);
-        }
-
-        const baseAppPath = userInfoHelper.getBaseAppPath(data.baseAppPath);
-        const deviceInfo = await userInfoHelper.getDeviceInfo(data.deviceIp);
-
-        const hostAppId = hostAppHelper.getHostAppId(baseAppPath);
-        const deviceName = deviceInfo.deviceName;
-        data.baseAppPath = baseAppPath;
-
-        watchHelper.openSocketServer(data, deviceInfo);
-        // appLaunchHelper.terminateApp(deviceName, hostAppId);
-
-        try {
-            data.isDebugMode
-                ? appLaunchHelper.launchDebugMode(
-                      deviceName,
-                      hostAppId,
-                      data.deviceIp
-                  )
-                : appLaunchHelper.launchApp(deviceName, hostAppId);
-        } catch (e) {
-            logger.log(e);
-            util.exit();
-        }
+    if (option !== undefined) {
+      await supportDeviceIpOption(data, option);
     }
+
+    const baseAppPath = userInfoHelper.getBaseAppPath(data.baseAppPath);
+    const deviceInfo = await userInfoHelper.getDeviceInfo(
+      data.deviceIp,
+      data.deviceConnectPort
+    );
+
+    const hostAppId = hostAppHelper.getHostAppId(baseAppPath);
+    const deviceName = deviceInfo.deviceName;
+    data.baseAppPath = baseAppPath;
+
+    watchHelper.openSocketServer(data, deviceInfo);
+    // appLaunchHelper.terminateApp(deviceName, hostAppId);
+
+    try {
+      data.isDebugMode
+        ? appLaunchHelper.launchDebugMode(deviceName, hostAppId, data.deviceIp)
+        : appLaunchHelper.launchApp(deviceName, hostAppId);
+    } catch (e) {
+      logger.log(e);
+      util.exit();
+    }
+  }
 };
 
 async function supportDeviceIpOption(data, option) {
-    const optionDeviceIp = util.parseDeviceIp(option);
-    if (optionDeviceIp === null && typeof option !== 'boolean') {
-        logger.error(
-            chalk.red(
-                `Invalid Type of cli option. Please retry with correct type. ex) deviceIp=0.0.0.0`
-            )
-        );
-        util.exit();
-    }
+  const optionDeviceIp = util.parseDeviceIp(option);
+  if (optionDeviceIp === null && typeof option !== 'boolean') {
+    logger.error(
+      chalk.red(
+        `Invalid Type of cli option. Please retry with correct type. ex) deviceIp=0.0.0.0`
+      )
+    );
+    util.exit();
+  }
 
-    if (optionDeviceIp) {
-        data.deviceIp = optionDeviceIp;
-        logger.log(
-            chalk.cyanBright(`WITs tries connecting with ${optionDeviceIp}\n`)
-        );
-        await userInfoHelper.updateLatestUserAnswer({
-            deviceIp: optionDeviceIp
-        });
-    }
+  if (optionDeviceIp) {
+    data.deviceIp = optionDeviceIp;
+    logger.log(
+      chalk.cyanBright(`WITs tries connecting with ${optionDeviceIp}\n`)
+    );
+    await userInfoHelper.updateLatestUserAnswer({
+      deviceIp: optionDeviceIp
+    });
+  }
 }
